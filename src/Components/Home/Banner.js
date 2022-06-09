08f517ee9497c1d74f07c3bf0abe5bc86cf610dd
arfat132 <arfatbegum90@gmail.com>
2022-06-09 17:36:46 +0300

    banner completed

--- src/Components/Home/Banner.js
+++ src/Components/Home/Banner.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import Typewriter from "typewriter-effect";
 
 const Banner = () => {
     return (
-        <>
-        </>
+        <div class="hero min-h-screen bg-[url(https://i.ibb.co/HV63Lv9/12-min.jpg)]">
+                        <div class="hero-content text-center text-neutral-content">
+                <div>
+                    <h1 className='text-2xl text-white font-medium'>Hello</h1>
+                    <h1 className='text-5xl font-bold mt-3 text-white'>I'AM ARFAT BEGUM</h1>
+                    <h1 className='text-pink-700 text-7xl font-bold mt-3'>
+                        <Typewriter
+                            onInit={(typewriter) => {
+                                typewriter
+                                    .typeString("Frontend Developer")
+                                    .pauseFor(1000)
+                                    .deleteAll()
+                                    .typeString("Web Developer")
+                                    .pauseFor(1000)
+                                    .deleteAll()
+                                    .typeString("React Js Developer")
+                                    .pauseFor(1000)
+                                    .deleteAll()
+                                    .typeString("Frontend Developer")
+                                    .pauseFor(1000)
+                                    .deleteAll()
+                                    .typeString("Web Developer")
+                                    .start();
+                            }}
+                        />
+                    </h1>
+                </div>
+            </div>
+        </div>
 
     );
 };
